d8f86fa8bccafbe99c5399ea1cd1d2944946accf
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2024-11-26 16:49:58 +0545

    basics of C# and .NET

--- obj/Debug/net8.0/dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b393332c644daa02b5be4f3bac2c1bee98449e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d17107ac58e8b011f8db5681dbe8ba7c97b93a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
