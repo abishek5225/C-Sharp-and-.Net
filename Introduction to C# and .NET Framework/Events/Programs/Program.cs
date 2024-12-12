13aaaed5851ef0897027e11c379e8b7a3d44880f
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2024-12-12 22:00:44 +0545

    updated

---
+++ Introduction to C# and .NET Framework/Events/Programs/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Doorbell{
+    public delegate void RingEventHandler(string message);
+
+    public event RingEventHandler Ring;
+
+    public void PressedButton(){
+        Console.Writeline("Doorbell is pressed");
+
+        Ring?Invoke("Someone is Ringing doorbell");
+    }
+}
+public class AlertSystem{
+    public void OnRing(string message){
+        Console.WriteLine($"Alert: {message}");
+    }
+}
+public class Program{
+    public static void Main(){
+        Doorbell doorbell= new Doorbell();
+
+        AlertSystem alert=new AlertSystem();
+
+        doorbell.Ring +=alert.OnRing;
+        doorbell.PressedButton();
+    }
+}
