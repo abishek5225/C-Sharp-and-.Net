e0acd4d1c260ccd5c199ed1d5c0899242ad07b13
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2024-12-12 11:24:02 +0545

    updated

---
+++ Introduction to C# and .NET Framework/Delegates/Programs/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Delegatesprogram{
+    class Program {
+
+        public delegate void addnum(int a, int b);
+        public delegate void subnum(int a, int b);
+
+        public void sum(int a, int b){
+            Console.WriteLine("Sum:"a+b);
+        }
+
+        public void subtract(int a, int b){
+            Console.WriteLine("Difference: "a,b);
+        }
+
+        public static void Main(){
+            Program obj= new Program();
+
+            addnum delobj=new addnum()
+        }
+    }
+}
